c025d4d431a181536eb3e1f74cbbaf29529691d8
slippedandmissed <gbt505@hotmail.com>
2022-01-25 14:13:22 +0000

    Added basic chat window

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let socket = io("http://localhost:3000");
+
+socket.on("connection", () => {
+    console.log("Connected to socket");
+});
+
+socket.on("answer", (payload) => {
+    addMessageToChat("Chatbot", payload);
+});
+
+function validateMessage() {
+    const textBox = document.querySelector("#text-box");
+    const valid = !!textBox.value;
+    document.querySelector("#send-button").disabled = !valid;
+}
+
+async function sendMessage(event) {
+    event.preventDefault();
+    const textBox = document.querySelector("#text-box");
+
+    addMessageToChat("You", textBox.value);
+    socket.emit("question", textBox.value);
+
+    textBox.value = "";
+}
+
+function addMessageToChat(sender, body) {
+    const chat = document.querySelector("#chat");
+    const template = document.querySelector("#message-template");
+    const messageDiv = template.cloneNode(true);
+    messageDiv.id = "";
+    messageDiv.querySelector(".sender").innerText = sender;
+    messageDiv.querySelector(".body").innerText = body;
+    chat.appendChild(messageDiv);
+}
